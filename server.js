f62ed14a3d2bf2e64a8414eae44e4dff4389d75e
kentleow1993 <kentleow1993@gmail.com>
2026-07-15 21:48:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.send(`
     <html>
       <head>
+        <meta charset="UTF-8">
         <meta http-equiv="refresh" content="0; url=https://www.4win.vip/myr/home">
       </head>
       <body>
-        Opening...
-      </body>
+        Opening...</body>
     </html>
   `);
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
