c4592a86fd9d175b8d06e32fce9d0a01ebc7f8a7
kentleow1993 <kentleow1993@gmail.com>
2026-07-15 22:27:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,48 @@ const app = express();
 
 app.get("*", (req, res) => {
   res.send(`
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="0; url=https://www.4win.vip/myr/home">
-      </head>
-      <body>
-        Opening...</body>
-    </html>
-  `);
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>4Win</title>
+
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
+
+<style>
+html,body{
+margin:0;
+padding:0;
+width:100%;
+height:100%;
+overflow:hidden;
+}
+
+iframe{
+border:none;
+width:100%;
+height:100%;
+}
+</style>
+
+</head>
+
+<body>
+
+<script>
+Telegram.WebApp.ready();
+Telegram.WebApp.expand();
+</script>
+
+<iframe src="https://www.4win.vip/myr/home"></iframe>
+
+</body>
+</html>
+`);
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+app.listen(PORT,"0.0.0.0",()=>{
+console.log("Running...");
 });
